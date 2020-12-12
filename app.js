0d07896d74cac2368fc9ffb0df05e597b96e8eb8
MadhushreeKunder <madhushreekunder@gmail.com>
2020-12-12 22:08:41 +0530

    completed styling

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ var btnClicked =  document.querySelector("#btn-click");
 var inputText = document.querySelector("#txt-input");
 var outputText = document.querySelector("#output");
 
-var serverURL= "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
-// var serverURL="https://api.funtranslations.com/translate/minion.json";
+// var serverURL= "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+var serverURL="	https://api.funtranslations.com/translate/morse.json";
 
 function errorHandler(error){
     console.log("error occured!", error)
