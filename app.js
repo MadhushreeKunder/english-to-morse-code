2b7b69c244086f4dc34939032ecf71dc07163bdd
MadhushreeKunder <madhushreekunder@gmail.com>
2021-10-07 22:44:06 +0530

    removed morse to eng section

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
 var btnClicked1 =  document.querySelector("#btn-click1");
-var btnClicked2 =  document.querySelector("#btn-click2");
 
 var inputText = document.querySelector("#txt-input");
 var outputText = document.querySelector("#output");
 
 // var serverURL= "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
-// var serverURL1 = "https://api.funtranslations.com/translate/morse.json";
-var serverURL1 = "	http://api.funtranslations.com/translate/morse/audio.json";
-var serverURL2 = "http://api.funtranslations.com/translate/morse2english.json";
+var serverURL1 = "https://api.funtranslations.com/translate/morse.json";
 
 function errorHandler(error){
     console.log("error occured!", error)
@@ -18,9 +15,6 @@ function getTranslatedURL1(text){
     return serverURL1 + "?" + "text=" + text
 }
 
-function getTranslatedURL2(text){
-    return serverURL2 + "?" + "text=" + text
-}
 
 function clickHandler1 (){
     var textInput = inputText.value;
@@ -33,16 +27,5 @@ function clickHandler1 (){
     .catch(errorHandler)
 };
 
-function clickHandler2 (){
-    var textInput = inputText.value;
-    fetch(getTranslatedURL2(textInput))
-    .then(response => response.json())
-    .then(json=> {
-        var translatedText = json.contents.translated;
-        outputText.innerText = translatedText; 
-        })
-    .catch(errorHandler)
-};
 
 btnClicked1.addEventListener("click", clickHandler1);
-btnClicked2.addEventListener("click", clickHandler2);
